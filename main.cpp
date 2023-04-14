d51d10a17d24d923975c4f9971fb964cc83f2ec9
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-04-14 15:43:39 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<Windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	OutputDebugStringA("kk\n");
+	return 0;
+}
---
+++ mian.cpp
@@ -0,0 +1,7 @@
+#include<Windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	OutputDebugStringA("kkkk\n");
+	return 0;
+}
