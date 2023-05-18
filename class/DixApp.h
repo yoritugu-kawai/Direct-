90e25d3f6b31de7f8be5b1698f96e0a0f699ea41
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-05-19 02:43:31 +0900

    ;

--- class/DixApp.h
+++ class/DixApp.h
@@ -1,14 +1,20 @@
 #pragma once
+#include<Windows.h>
+#include <cstdint>
+#include<string>
+#include<format>
 #include<d3d12.h>
 #include<dxgi1_6.h>
 #include<cassert>
-#include<string>
-#include<format>
-#include<Windows.h>
-#include <cstdint>
+#include<dxgidebug.h>
+#include<dxcapi.h>
+
 
 #pragma comment(lib,"dxguid.lib")
 #pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"dxcompiler.lib")
+
 class DixApp
 {
 public:
