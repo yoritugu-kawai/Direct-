ab197e3ca4280ee36db5afab087fec83d5443fbf
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-06-12 15:44:40 +0900

    nh

--- Myclass/Polygon.cpp
+++ Myclass/Polygon.cpp
@@ -1,29 +1,29 @@
-//#include"Polygon.h"
-//
-//Polygon::Polygon()
-//{
-//}
-//
-//Polygon::~Polygon()
-//{
-//}
-//
-//void Polygon::Initialize()
-//{
-//	//DXCの初期化
-//	IDxcUtils* dxcUtils = nullptr;
-//	IDxcCompiler3* dxcCompiler = nullptr;
-//	hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
-//	assert(SUCCEEDED(hr));
-//	hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
-//	assert(SUCCEEDED(hr));
-//	IDxcIncludeHandler* includeHandler = nullptr;
-//	hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
-//	assert(SUCCEEDED(hr));
-//}
-//
-//void Polygon::Drow()
-//{
-//
-//
-//}
+#include"Polygon.h"
+
+Polygon::Polygon()
+{
+}
+
+Polygon::~Polygon()
+{
+}
+
+void Polygon::Initialize()
+{
+	//DXCの初期化
+	IDxcUtils* dxcUtils = nullptr;
+	IDxcCompiler3* dxcCompiler = nullptr;
+	hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
+	assert(SUCCEEDED(hr));
+	hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+	assert(SUCCEEDED(hr));
+	IDxcIncludeHandler* includeHandler = nullptr;
+	hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+	assert(SUCCEEDED(hr));
+}
+
+void Polygon::Drow()
+{
+
+
+}
--- Myclass/Polygon.h
+++ Myclass/Polygon.h
@@ -1,18 +1,28 @@
-//#pragma once
-//#include<dxcapi.h>
-//#include"WinApp.h"
-//
-//#pragma comment(lib,"dxcompiler.lib")
-//
-//class Polygon
-//{
-//public:
-//	Polygon();
-//	~Polygon();
-//	void Initialize();
-//	void Drow();
-//
-//private:
-//	HRESULT hr;
-//};
-//
+#pragma once
+#include<Windows.h>
+#include <cstdint>
+#include<string>
+#include<format>
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<cassert>
+#include<dxgidebug.h>
+#include"WinApp.h"
+#include<dxcapi.h>
+
+#pragma comment(lib,"dxguid.lib")
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"dxcompiler.lib")
+class Polygon
+{
+public:
+	Polygon();
+	~Polygon();
+	void Initialize();
+	void Drow();
+
+private:
+	HRESULT hr;
+};
+
