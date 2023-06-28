55e1aa59a65f983f8d91ba23fba18d93d2dcc83a
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-06-28 14:22:41 +0900

    lll

--- Odject3d.PS.hlsl
+++ Odject3d.PS.hlsl
@@ -12,7 +12,7 @@ struct PixelShaderOutput {
 
 PixelShaderOutput main() {
 	PixelShaderOutput output;
-	output.color = float32_t4(1.0, 0.0, 0.0, 1.0);
+	output.color = float32_t4(1.0f, 0.0f, 0.0f, 1.0f);
 
 	return output;
 }
