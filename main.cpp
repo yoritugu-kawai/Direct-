e45fd479e46187dad1944097a08593d7d156faeb
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-07-24 14:33:11 +0900

    sss

--- main.cpp
+++ main.cpp
@@ -21,20 +21,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	winApp_->Initialize();
 	DxCommon* dxCommon_ = new DxCommon;
 	dxCommon_->Initialize(winApp_->Width(), winApp_->Height(), winApp_->Gethwnd());
+
 	const int max = 6;
 	
-
 	TrianglePropaty triangle[max];
 
 	triangle[0] =
 	{
-
 	    {-0.8f,-0.0f,0.0f,1.0f},
 	    {-0.6f,0.5f,0.0f,1.0f},
 		{-0.5f,-0.0f,0.0f,1.0f},
 		{0x783964ff}
-
-
 	};
 
 	triangle[1] =
@@ -43,8 +40,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{-0.35f,0.5f,0.0f,1.0f},
 		{-0.2f,-0.0f,0.0f,1.0f},
 	    {0xff44ffff}
-
-
 	};
 
 	triangle[2] =
@@ -53,18 +48,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{0.1f,0.5f,0.0f,1.0f},
 		{0.2f,-0.0f,0.0f,1.0f},
 		{0x22ffffff}
-
-
 	};
+
 	triangle[3] =
 	{
 		{-0.1f,-0.3f,0.0f,1.0f},
 		{0.1f,-0.1f,0.0f,1.0f},
 		{0.2f,-0.3f,0.0f,1.0f},
 		{0xff00ffff}
-
-
 	};
+
 	triangle[4] =
 	{
 		{-0.8f,-0.3f,0.0f,1.0f},
@@ -73,6 +66,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{0xffffafaf}
 
 	};
+
 	triangle[5] =
 	{
 		{-0.4f,-0.3f,0.0f,1.0f},
@@ -86,8 +80,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		polygon_[i] = new PolygoType;
 		polygon_[i]->Initiluze(dxCommon_);
 	}
+
 	//　メインループ
 	MSG msg{};
+
+
 	while (msg.message != WM_QUIT)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
@@ -108,7 +105,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				polygon_[i]->Draw(triangle[i].left, triangle[i].top, triangle[i].right, triangle[i].color);
 			}
 
-
 			dxCommon_->EndFrame();
 			//　ゲーム処理
 
@@ -117,7 +113,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	winApp_->Release();
 	dxCommon_->Release(winApp_->Gethwnd());
-
 
 	for (int i = 0; i < max; i++) {
 		delete polygon_[i];
