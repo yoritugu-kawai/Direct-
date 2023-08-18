d095a764f89b3278c0351b92b92930b1d6711bd7
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-19 00:36:07 +0900

    kk

---
+++ Myclass/type/Texture.cpp
@@ -0,0 +1,15 @@
+#include "Texture.h"
+
+void Texture::Initiluze()
+{
+	CoInitializeEx(0, COINIT_MULTITHREADED);
+	CoUninitialize();
+}
+
+void Texture::Update()
+{
+}
+
+void Texture::Draw()
+{
+}
---
+++ Myclass/type/Texture.h
@@ -0,0 +1,13 @@
+#pragma once
+#include"../dase/WinApp.h"
+#include"../dase/Dxcommen.h"
+#include"../math/mathStrt.h"
+class Texture
+{
+public:
+	void Initiluze();
+	void Update();
+	void Draw();
+private:
+
+};
