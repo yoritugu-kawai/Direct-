2e951b68e9429bd1640a4f3e679cb1096c80ea6a
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-23 03:01:12 +0900

    dd

--- Myclass/math/mathStrt.h
+++ Myclass/math/mathStrt.h
@@ -34,6 +34,14 @@ struct Vector4
 struct   Matrix4x4 {
 	float m[4][4];
 };
+
+struct  Transfom
+{
+	Vector3 scale;
+	Vector3 rotate;
+	Vector3 translate;
+};
+
 /// <summary>
 /// 数学
 /// </summary>
@@ -47,11 +55,4 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 Matrix4x4 MakeRotateXYZMatrix(float radianX, float radianY, float radianZ);
 Matrix4x4 MakeTranslateMatrix(const Vector3 translate);
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
-
-struct  Transfom
-{
-	Vector3 scale;
-	Vector3 rotate;
-	Vector3 translate;
-};
 
