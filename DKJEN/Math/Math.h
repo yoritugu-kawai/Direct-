8528eaa294d112ceb93ab6ba7597b8a8c9e77b44
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-23 03:01:37 +0900

    ddd

--- DKJEN/Math/Math.h
+++ DKJEN/Math/Math.h
@@ -13,3 +13,47 @@
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"dxcompiler.lib")
+
+struct Vector2 {
+	float x;
+	float y;
+};
+struct  Vector3
+{
+	float x;
+	float y;
+	float z;
+
+};
+struct Vector4
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+struct   Matrix4x4 {
+	float m[4][4];
+};
+
+struct  Transfom
+{
+	Vector3 scale;
+	Vector3 rotate;
+	Vector3 translate;
+};
+
+/// <summary>
+/// 数学
+/// </summary>
+/// <returns></returns>
+Matrix4x4 MakeIdentity4x4();
+Matrix4x4 MakeScaleMatrix(const Vector3 scale);
+Matrix4x4 MakeRotateXMatrix(float radian);
+Matrix4x4 MakeRotateYMatrix(float radian);
+Matrix4x4 MakeRotateZMatrix(float radian);
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 MakeRotateXYZMatrix(float radianX, float radianY, float radianZ);
+Matrix4x4 MakeTranslateMatrix(const Vector3 translate);
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
