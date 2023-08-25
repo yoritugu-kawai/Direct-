ec7a8fbdb07b1e633fa95d7622d00f13a58b7deb
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-26 02:03:14 +0900

    ss

---
+++ DKJEN/Type/Polygon.cpp
@@ -0,0 +1,13 @@
+#include "Polygon.h"
+
+void Polygon::Initialize(Vector4 pos, Vector4 Color)
+{
+}
+
+void Polygon::Draw()
+{
+}
+
+void Polygon::Release()
+{
+}
--- DKJEN/Type/Polygon.h
+++ DKJEN/Type/Polygon.h
@@ -1,1 +1,13 @@
 #pragma once
+#include"../Math/Math.h"
+class Polygon
+{
+public:
+	void Initialize( Vector4 pos, Vector4 Color);
+	void Draw();
+	void Release();
+private:
+	Vector4 CenterPos_ = { 0.0f,0.0f,0.0f };
+	Vector4 Color_ = { 0.0f,0.0f,1.0f,1.0f };
+	const float size = 0.1f;
+};
