3a637f8146e3564efb334867e0b8b86320d6b8cc
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-26 02:26:33 +0900

    kk

--- DKJEN/Type/Polygon.cpp
+++ DKJEN/Type/Polygon.cpp
@@ -1,13 +1,62 @@
 #include "Polygon.h"
 
-void Polygon::Initialize(Vector4 pos, Vector4 Color)
+void PolygonType::Initialize(Vector4 pos, Vector4 Color)
+{
+	Vertex=
+}
+
+
+void PolygonType::Draw()
 {
 }
 
-void Polygon::Draw()
+void PolygonType::Release()
 {
 }
 
-void Polygon::Release()
+ID3D12Resource* PolygonType::CreateBufferResource(size_t sizeInbyte)
 {
+	ID3D12Device* device = DxCommon::GetInstance()->GetDevice();
+	ID3D12Resource* RssultResource;
+	//頂点リソース用のヒープの設定
+	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
+
+	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; //UploadHeapを使う
+
+	//頂点リソースの設定
+	D3D12_RESOURCE_DESC ResourceDesc{};
+
+	//バッファリソース。テクスチャの場合はまた別の設定をする
+	ResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	ResourceDesc.Width = sizeInbyte; //リソースのサイズ。今回はvector4を3頂点分
+
+	//バッファの場合はこれらは1にする決まり
+	ResourceDesc.Height = 1;
+	ResourceDesc.DepthOrArraySize = 1;
+	ResourceDesc.MipLevels = 1;
+	ResourceDesc.SampleDesc.Count = 1;
+
+	//バッファの場合はこれにする決まり
+	ResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	HRESULT hr;
+	hr = device->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE,
+		&ResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&RssultResource));
+	assert(SUCCEEDED(hr));
+
+	return RssultResource;
+}
+
+D3D12_VERTEX_BUFFER_VIEW PolygonType::VertexCreateBufferView(size_t sizeInbyte, ID3D12Resource* Resource, int size)
+{
+
+	D3D12_VERTEX_BUFFER_VIEW resultBufferView = {};
+
+	resultBufferView.BufferLocation = Resource->GetGPUVirtualAddress();
+
+	//使用するリソースのサイズは頂点3つ分のサイズ
+	resultBufferView.SizeInBytes = UINT(sizeInbyte);
+
+	//1頂点あたりのサイズ
+	resultBufferView.StrideInBytes = UINT(sizeInbyte / size);
+	return resultBufferView;
 }
--- DKJEN/Type/Polygon.h
+++ DKJEN/Type/Polygon.h
@@ -1,13 +1,20 @@
 #pragma once
 #include"../Math/Math.h"
-class Polygon
+#include"../Base/DxCommon.h"
+class PolygonType
 {
 public:
 	void Initialize( Vector4 pos, Vector4 Color);
 	void Draw();
 	void Release();
+	//
+	static ID3D12Resource* CreateBufferResource(size_t sizeInbyte);
+	static D3D12_VERTEX_BUFFER_VIEW VertexCreateBufferView(size_t sizeInbyte, ID3D12Resource* Resource, int size);
+
 private:
 	Vector4 CenterPos_ = { 0.0f,0.0f,0.0f };
 	Vector4 Color_ = { 0.0f,0.0f,1.0f,1.0f };
 	const float size = 0.1f;
+	DxCommon dxCommmon_;
+	ID3D12Resource* Vertex;
 };
