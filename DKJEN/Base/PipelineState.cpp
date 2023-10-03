dca9415b07b9f9ff3d4bef03db53ca2dd9316c52
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-10-03 15:33:28 +0900

    ss

--- DKJEN/Base/PipelineState.cpp
+++ DKJEN/Base/PipelineState.cpp
@@ -99,7 +99,7 @@ void PipelineState::ShapePSO()
 	D3D12_RASTERIZER_DESC rasterizerDesc{};
 
 	//裏面（時計回り）を表示しない
-	rasterizerDesc.CullMode = D3D12_CULL_MODE_NONE;
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_FRONT;
 	//三角形の中を塗りつぶす
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
